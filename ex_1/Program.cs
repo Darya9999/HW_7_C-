7014a07254a32e69acdd15d9cdd4a8ddf107a96e
Darya <darya.ogurtsova.99@mail.ru>
2022-09-02 17:38:08 +0300

    добавлено задание 1

---
+++ ex_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int m = 3;
+int n = 4;
+double[,] arr = new double[m,n];
+Random rand = new Random(DateTime.Now.Millisecond);
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+arr[i, j] = Math.Round(rand.NextDouble() * 20 , 2 );
+}
+}
+string otv = "";
+
+for (int i = 0; i < m; i++) {
+otv = "";
+for (int j = 0; j < n; j++){
+otv += arr[i, j];
+if (j<n-1) {
+otv += " ";
+}
+}
+Console.WriteLine(otv);
+}
